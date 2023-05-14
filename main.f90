566073e6c3312088c3ea3ff583d64f2d460e4b22
DESKTOP-5E7409A\Alex <Alex>
2023-05-14 15:33:09 +0300

    14.05.23

--- main.f90
+++ main.f90
@@ -3,22 +3,30 @@ program p
 
    implicit none
 
-   integer(4), parameter :: n = 1000
-   real(8) :: dz, z(n)
+   integer(4), parameter :: n = 663
+   real(4) :: dz, z(n), zex
    complex(8) :: u(n)
    integer(4) i, l
 
-   call init_uinterpol(n)
+   !call init_uinterpol(n)
 
-   dz = 185.5/(n - 1)
+   !dz = 185.5/(n - 1)
+   !do i = 1, 1000
+   !   z(i) = -8.5 + (i - 1)*dz
+   !   u(i) = uval(z(i))
+   !end do
 
-   do i = 1, 1000
-      z(i) = -8.5 + (i - 1)*dz
-      u(i) = ucalc(z(i))
+   !zex = 11.258330249197702
+   zex = 185.5
+   dz = zex/(n - 1)
+   do i = 1, n
+      z(i) = (i - 1)*dz
+      u(i) = squval(dble(z(i)))
+      z(i) = z(i) - 8.5
    end do
 
    open (1, file='interpol.dat')
-   do i = 1, 1000
+   do i = 1, n
       write (1, '(3f12.6)') z(i), dreal(u(i)), dimag(u(i))
    end do
    close (1)
--- uinterpol.f90
+++ uinterpol.f90
@@ -4,6 +4,7 @@ module uinterpol
    implicit none
 
    !real(c_double), private :: za(663), rea(663), ima(663)
+   real(c_double), private :: zex
    include 'z.inc'
    include 're.inc'
    include 'im.inc'
@@ -11,26 +12,72 @@ module uinterpol
 
 contains
 
-   function ucalc(z)
+   function squval(zz)
 
       implicit none
 
-      complex(c_double_complex) ucalc
+      real(c_double), intent(in) :: zz
+
+      complex(c_double_complex) squval
+      real(c_double) z, re, im, dz, z1
+      integer(c_int) l
+
+      zex = 185.5
+      dz = 0.28011
+      z = zz/zex*185.5
+      l = z/dz
+
+      if (l .eq. 0) then
+         l = 2
+      elseif (l .ge. 662) then
+         l = 662
+      else
+         if ((z - za(l)) .gt. 0.5*dz) l = l + 1
+      end if
+
+      z1 = za(l)
+      z = z - 8.5d0
+
+      re = rea(l - 1) + ((-rea(l - 1) + rea(l))*(dz + z - z1))/dz + ((rea(l - 1)/2.0d0 - rea(l) + &
+                                                                      rea(l + 1)/2.0d0)*(z - z1)*(dz + z - z1))/dz/dz
+      im = ima(l - 1) + ((-ima(l - 1) + ima(l))*(dz + z - z1))/dz + ((ima(l - 1)/2.0d0 - ima(l) + &
+                                                                      ima(l + 1)/2.0d0)*(z - z1)*(dz + z - z1))/dz/dz
+      !!!NE RABOTAET
+      !re = ((rea(l - 1) - 2*rea(l) + rea(l + 1))*z**2)/(2.*dz**2) &
+      !     + (z*(dz*(-rea(l - 1) + rea(l + 1)) - 2*(rea(l - 1) - 2*rea(l) + rea(l + 1))*z1))/(2.*dz**2) + &
+      !     -(2*dz**2*rea(l) + dz*(rea(l - 1) - rea(l + 1))*z1 + (rea(l - 1) - 2*rea(l) + rea(l + 1))*z1**2)/(2.*dz**2)
+      !im = ((ima(l - 1) - 2*ima(l) + ima(l + 1))*z**2)/(2.*dz**2) + &
+      !     (z*(dz*(-ima(l - 1) + ima(l + 1)) - 2*(ima(l - 1) - 2*ima(l) + ima(l + 1))*z1))/(2.*dz**2) + &
+      !     -(2*dz**2*ima(l) + dz*(ima(l - 1) - ima(l + 1))*z1 + (ima(l - 1) - 2*ima(l) + ima(l + 1))*z1**2)/(2.*dz**2)
+
+      squval = dcmplx(re, im)
+
+   end function squval
+
+   function linuval(zz)
+
+      implicit none
+
+      real(c_double), intent(in) :: zz
+
+      complex(c_double_complex) linuval
       real(c_double) z, re, im, d
       integer(c_int) l
 
-      l = (z + 8.5)/0.28021 + 1
-
+      !zex = 11.258330249197702
+      zex = 185.5
+      z = zz/zex*185.5 - 8.5
+      l = (z + 8.5)/0.280211 + 1
       d = z - za(l)
 
       !print *, z, l, d
 
-      if (d .gt. 0.0) then
+      if (d .gt. 0.0 .and. l /= 663) then
          re = (rea(l)*za(l + 1) - rea(l + 1)*za(l))/(za(l + 1) - za(l)) + &
               (rea(l + 1) - rea(l))/(za(l + 1) - za(l))*z
          im = (ima(l)*za(l + 1) - ima(l + 1)*za(l))/(za(l + 1) - za(l)) + &
               (ima(l + 1) - ima(l))/(za(l + 1) - za(l))*z
-      else if (d .lt. 0.0) then
+      else if (d .lt. 0.0 .and. l /= 1) then
          re = (rea(l - 1)*za(l) - rea(l)*za(l - 1))/(za(l) - za(l - 1)) + &
               (rea(l) - rea(l - 1))/(za(l) - za(l - 1))*z
          im = (ima(l - 1)*za(l) - ima(l)*za(l - 1))/(za(l) - za(l - 1)) + &
@@ -40,48 +87,48 @@ contains
          im = ima(l)
       end if
 
-      ucalc = dcmplx(re, im)
+      linuval = dcmplx(re, im)
 
-   end function ucalc
+   end function linuval
 
-   subroutine init_uinterpol(n)
-
-      implicit none
-
-      integer(c_int) :: n, i
-      real(c_double) d1, d2, d3
-
-      open (1, file='table.dat', status='old')
-      do i = 1, 663
-         read (1, '(6f12.6)') za(i), d1, d2, d3, rea(i), ima(i)
-         !write (*, '(6f12.6)') za(i), d1, d2, d3, rea(i), ima(i)
-      end do
-      close (1)
-
-      open (1, file='z.inc')
-      write (1, '(a)') 'real(c_double), private :: za(663) =(/ &'
-      do i = 1, 662
-         write (1, '(f12.6,a)') za(i), ', &'
-      end do
-      write (1, '(f12.6,a)') za(663), '/)'
-      close (1)
-
-      open (1, file='re.inc')
-      write (1, '(a)') 'real(c_double), private :: rea(663) =(/ &'
-      do i = 1, 662
-         write (1, '(f12.6,a)') rea(i), ', &'
-      end do
-      write (1, '(f12.6,a)') rea(663), '/)'
-      close (1)
-
-      open (1, file='im.inc')
-      write (1, '(a)') 'real(c_double), private :: ima(663) =(/ &'
-      do i = 1, 662
-         write (1, '(f12.6,a)') ima(i), ', &'
-      end do
-      write (1, '(f12.6,a)') ima(663), '/)'
-      close (1)
-
-   end subroutine init_uinterpol
+   !subroutine init_uinterpol(n)
+   !
+   !   implicit none
+   !
+   !   integer(c_int) :: n, i
+   !   real(c_double) d1, d2, d3
+   !
+   !   open (1, file='table.dat', status='old')
+   !   do i = 1, 663
+   !      read (1, '(6f12.6)') za(i), d1, d2, d3, rea(i), ima(i)
+   !      !write (*, '(6f12.6)') za(i), d1, d2, d3, rea(i), ima(i)
+   !   end do
+   !   close (1)
+   !
+   !   open (1, file='z.inc')
+   !   write (1, '(a)') 'real(c_double), private :: za(663) =(/ &'
+   !   do i = 1, 662
+   !      write (1, '(f12.6,a)') za(i), ', &'
+   !   end do
+   !   write (1, '(f12.6,a)') za(663), '/)'
+   !   close (1)
+   !
+   !   open (1, file='re.inc')
+   !   write (1, '(a)') 'real(c_double), private :: rea(663) =(/ &'
+   !   do i = 1, 662
+   !      write (1, '(f12.6,a)') rea(i), ', &'
+   !   end do
+   !   write (1, '(f12.6,a)') rea(663), '/)'
+   !   close (1)
+   !
+   !   open (1, file='im.inc')
+   !   write (1, '(a)') 'real(c_double), private :: ima(663) =(/ &'
+   !   do i = 1, 662
+   !      write (1, '(f12.6,a)') ima(i), ', &'
+   !   end do
+   !   write (1, '(f12.6,a)') ima(663), '/)'
+   !   close (1)
+   !
+   !end subroutine init_uinterpol
 
 end module uinterpol
